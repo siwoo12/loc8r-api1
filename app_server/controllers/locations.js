de391fb59f2910fe4a64f69449ac44e751c0a4b5
siwoo12 <isiu0782@gmail.com>
2024-12-08 06:32:53 +0900

    Update locations.js

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,7 @@ const apiOptions = {
   server: 'http://localhost:3000'
 };
 if(process.env.NODE_ENV === 'production'){
-  apiOptions.server  = 'https://loc8r-api-c3li.onrender.com';
+  apiOptions.server  = 'https://loc8r-api1-1dsu.onrender.com';
 }
 
 const homelist = (req, res) => {
